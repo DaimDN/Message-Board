981c6e6123ce93a5a2a482350b5bcaf43d71a1e7
daimdn <abdul.daim26@gmail.com>
2020-12-16 22:21:24 +0000

    Backend Completed

--- Server/src/main/java/com/jpmorgan/board/Controllers/Controllers.java
+++ Server/src/main/java/com/jpmorgan/board/Controllers/Controllers.java
@@ -13,16 +13,13 @@ import java.util.Date;
 
 @Controller
 @RestController
+
 public class Controllers {
 
     @Autowired
     private ArrayList arrayList ;
 
-
-
-
-
-
+    @CrossOrigin("http://localhost:3000")
     @GetMapping(path = "/", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public LandingPage welcome(LandingPage message){
@@ -30,17 +27,36 @@ public class Controllers {
         return title;
     }
 
-
+   
     @GetMapping(path = "/dummy", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public ArrayList dummy(){
-        Message message = new Message("abdul.daim26@gmail.com", "We", "s");
-        Message message2 = new Message("abdul.daim26@gmail.com", "We", "s");
+        String dummyMessage = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+
+        StringBuffer stringBuffer = new StringBuffer();
+
+        if(dummyMessage.length() <= 500){
+            stringBuffer.append(dummyMessage);
+        }
+        else{
+            stringBuffer.append(dummyMessage.substring(0, 500));
+
+        }
+        String newMessage = stringBuffer.toString();
+
+
+
+        Message message = new Message("john.doe@gmail.com", newMessage, "16/12/2020");
+        Message message2 = new Message("joe.bloggs@gmail.com", newMessage, "16/12/2020");
+        Message message3 = new Message("jimmy@gmail.com", newMessage, "16/12/2020");
+
+
         arrayList.add(message);
         arrayList.add(message2);
+        arrayList.add(message3);
         return arrayList;
     }
-
+    @CrossOrigin("http://localhost:3000")
     @PostMapping(path="/add", consumes = "application/json", produces = "application/json")
     @ResponseStatus(HttpStatus.OK)
     public Message createMessage(@RequestBody  Message msg){
@@ -49,8 +65,18 @@ public class Controllers {
         String message = msg.getMessage();
         String key = msg.getKey();
 
-        arrayList.add(msg);
-        
+        StringBuilder stringBuilder = new StringBuilder();
+
+        if(message.length() <= 500){
+            stringBuilder.append(message);
+        }else{
+            stringBuilder.append(message.substring(0, 500));
+        }
+        String newMsg = stringBuilder.toString();
+
+        Message newMessage = new Message(email, newMsg, key);
+
+        arrayList.add(newMessage);
         return msg;
 
     }
